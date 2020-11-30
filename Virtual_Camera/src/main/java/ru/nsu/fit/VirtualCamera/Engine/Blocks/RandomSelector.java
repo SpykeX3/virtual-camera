0b811a681d631368c9913b074d91202d5c4ecef2
APlyusnin <a.plyusnin@g.nsu.ru>
2020-11-30 17:15:48 +0700

    experimenting with block syncronization, added possibility to instantiat emodules with list of args

---
+++ Virtual_Camera/src/main/java/ru/nsu/fit/VirtualCamera/Engine/Blocks/RandomSelector.java
@@ -0,0 +1,58 @@
+package ru.nsu.fit.VirtualCamera.Engine.Blocks;
+
+import ru.nsu.fit.VirtualCamera.Engine.Frame;
+import ru.nsu.fit.VirtualCamera.Engine.FunctionalBlock;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RandomSelector extends FunctionalBlock
+{
+
+    List<Double> probability;
+    public RandomSelector(List<String> args) throws Exception
+    {
+        validateArgs(args);
+        int sum = 0;
+        probability = new LinkedList<>();
+        for (var x : args)
+            sum += Integer.parseInt(x);
+
+        int acc = 0;
+        for (var x : args)
+        {
+            sum += Integer.parseInt(x);
+            probability.add(acc * 1.0 / sum);
+        }
+    }
+
+
+    @Override
+    protected void validateArgs(List<String> args) throws Exception {
+        for (var x : args)
+        {
+            Integer.parseInt(x);
+        }
+    }
+
+    @Override
+    public Frame performWork() {
+        double val = Math.random();
+        int id = 0;
+        Frame frame = null;
+        for (var x : probability)
+        {
+            if (x <= val)
+            {
+                frame = inputFrames.get(id);
+            }
+            id++;
+        }
+        return frame;
+    }
+
+    @Override
+    protected void aftermath() {
+
+    }
+}
--- Virtual_Camera/src/test/java/ru/nsu/fit/VirtualCamera/Engine/Blocks/VerticalMirrorTest.java
+++ Virtual_Camera/src/test/java/ru/nsu/fit/VirtualCamera/Engine/Blocks/VerticalMirrorTest.java
@@ -14,41 +14,44 @@ public class VerticalMirrorTest {
     public void Test()
     {
         //OpenCV.loadShared();
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        FrameStream stream = new FrameStream();
-        FrameStream stream1 = new FrameStream();
+        try {
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        String file = "src/test/resources/orange.jpg";
-        Mat mat = Imgcodecs.imread(file);
+            FrameStream stream = new FrameStream();
+            FrameStream stream1 = new FrameStream();
 
-        Frame frame = new Frame(mat);
+            String file = "src/test/resources/orange.jpg";
+            Mat mat = Imgcodecs.imread(file);
 
-        VerticalMirror mirror = new VerticalMirror();
+            Frame frame = new Frame(mat);
 
-        mirror.addInputStream(stream);
-        mirror.addOutputStream(stream1);
-        stream.write(frame);
+            VerticalMirror mirror = new VerticalMirror(null);
 
-        Thread thread = new Thread(mirror);
+            mirror.addInputStream(stream);
+            mirror.addOutputStream(stream1);
+            stream.write(frame);
 
-        thread.start();
-        try
-        {
-            Thread.sleep(500);
-            mirror.kill();
-            Thread.sleep(500);
+            Thread thread = new Thread(mirror);
+
+            thread.start();
+            try {
+                Thread.sleep(500);
+                mirror.kill();
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Assert.fail();
+            }
+
+            Frame frame1 = stream1.read();
+            if (frame1 == null) {
+                Assert.fail();
+            }
+            Assert.assertNotEquals(mat, frame1.getMatrix());
+            Imgcodecs.imwrite("src/test/resources/test.jpg", frame1.getMatrix());
         }
-        catch (InterruptedException e)
+        catch (Exception e)
         {
             Assert.fail();
         }
-
-        Frame frame1 = stream1.read();
-        if (frame1 == null)
-        {
-            Assert.fail();
-        }
-        Assert.assertNotEquals(mat, frame1.getMatrix());
-        Imgcodecs.imwrite("src/test/resources/test.jpg", frame1.getMatrix());
     }
 }
