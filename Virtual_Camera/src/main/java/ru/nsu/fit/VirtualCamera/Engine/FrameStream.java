9530e91201a90d16142550c7adaac00cce52072f
SpykeX3 <spykex3@gmail.com>
2020-12-01 01:06:09 +0700

    fixed concurrency problems in FrameStream

--- Virtual_Camera/src/main/java/ru/nsu/fit/VirtualCamera/Engine/FrameStream.java
+++ Virtual_Camera/src/main/java/ru/nsu/fit/VirtualCamera/Engine/FrameStream.java
@@ -1,66 +1,46 @@
 package ru.nsu.fit.VirtualCamera.Engine;
+
 import java.io.BufferedInputStream;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 
+/** Class for frame stream */
+public class FrameStream implements FrameInputStream, FrameOutputStream {
 
-/**
- * Class for frame stream
- */
-public class FrameStream implements FrameInputStream, FrameOutputStream{
+  private BlockingQueue<Frame> data;
 
-    private Queue<Frame> data;
+  private int BUFFER_SIZE = 10;
+  private Semaphore freeSpace;
+  private Semaphore takenSpace;
 
-    private int BUFFER_SIZE = 10;
-    private Semaphore freeSpace;
-    private Semaphore takenSpace;
+  /** Create frame stream object. Should be used by manager */
+  public FrameStream() {
+    data = new LinkedBlockingQueue<>(BUFFER_SIZE);
+    freeSpace = new Semaphore(BUFFER_SIZE);
+    takenSpace = new Semaphore(0);
+  }
 
-    /**
-     * Create frame stream object. Should be used by manager
-      */
-    public FrameStream()
-    {
-        data = new LinkedList<>();
-        freeSpace = new Semaphore(BUFFER_SIZE);
-        takenSpace = new Semaphore(0);
+  public FrameStream(int BUFFER_SIZE) {
+    data = new LinkedBlockingQueue<>(BUFFER_SIZE);
+    this.BUFFER_SIZE = BUFFER_SIZE;
+    freeSpace = new Semaphore(BUFFER_SIZE);
+    takenSpace = new Semaphore(0);
+  }
+
+  @Override
+  public Frame read() throws InterruptedException {
+    return data.take();
+  }
+
+  @Override
+  public boolean write(Frame frame) throws InterruptedException {
+    if (frame == null) {
+      return false;
     }
-
-    public FrameStream(int BUFFER_SIZE)
-    {
-        data = new LinkedList<>();
-        this.BUFFER_SIZE = BUFFER_SIZE;
-        freeSpace = new Semaphore(BUFFER_SIZE);
-        takenSpace = new Semaphore(0);
-    }
-
-
-    @Override
-    public Frame read()
-    {
-        try {
-            takenSpace.acquire();
-            Frame res = data.poll();
-            freeSpace.release();
-            return res;
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-    }
-
-    @Override
-    public boolean write(Frame frame) {
-        try {
-            freeSpace.acquire();
-            data.add(frame);
-            takenSpace.release();
-            return true;
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-    }
+    data.put(frame);
+    return true;
+  }
 }
